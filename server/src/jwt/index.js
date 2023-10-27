841bbab138d2855bb0f80e5cf1da71dc6c94850d
Nguyen Truong Son <ntstruongson2k3@gmail.com>
2023-10-27 16:49:14 +0700

    create jwt

---
+++ server/src/jwt/index.js
@@ -0,0 +1,29 @@
+import jwt from "jsonwebtoken";
+
+const generateAccessToken = (customer) => {
+    return jwt.sign(
+        {
+            id: customer._id,
+            role: customer.role,
+            admin: customer.admin,
+        },
+        process.env.JWT_ACCESS_KEY,
+        {
+            expiresIn: "1h",
+        }
+    );
+};
+const generateRefreshToken = (customer) => {
+    return jwt.sign(
+        {
+            id: customer._id,
+            role: customer.role,
+            admin: customer.admin,
+        },
+        process.env.JWT_REFRESH_KEY,
+        {
+            expiresIn: "1d",
+        }
+    );
+};
+module.exports = { generateAccessToken, generateRefreshToken };
