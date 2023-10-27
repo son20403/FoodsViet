230f21b02239fc85109162b3784e7bc8c625a66a
hoai <hoai3k4s@gmail.com>
2023-10-27 17:42:40 +0700

    ok

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import { RouterProvider } from "react-router-dom";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { toast } from "react-toastify";
+import { router } from "./routes";
 
-const App = () => {
+function App() {
+  const { token } = useSelector((state) => state.auth);
+  const { errorGlobal, notifyGlobal } = useSelector((state) => state.global);
+  useEffect(() => {
+    if (errorGlobal) toast.error(errorGlobal);
+  }, [errorGlobal]);
+  useEffect(() => {
+    if (notifyGlobal) toast.success(notifyGlobal);
+  }, [notifyGlobal]);
   return (
-    <div >
-      <h1 className='text-red-500'>haha</h1>
-    </div>
+    <>
+      <RouterProvider router={router} />
+    </>
   );
-};
+}
 
 export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import 'react-toastify/dist/ReactToastify.css';
-import 'react-loading-skeleton/dist/skeleton.css'
-import App from './App'
-ReactDOM.createRoot(document.getElementById('root')).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "react-loading-skeleton/dist/skeleton.css";
+import { Provider } from "react-redux";
+import store from "./sagas/configureStore";
+import App from "./App";
+ReactDOM.createRoot(document.getElementById("root")).render(
   <>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
+    <ToastContainer
+      autoClose={1000}
+      style={{ zIndex: "9999999" }}
+    ></ToastContainer>
   </>
-)
-
+);
