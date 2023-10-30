401b30b956b083f3402956a98a13aba96a618f78
Nguyen Truong Son <ntstruongson2k3@gmail.com>
2023-10-30 09:09:31 +0700

    edit file contact

--- client/src/layout/common/BannerCommon.jsx
+++ client/src/layout/common/BannerCommon.jsx
@@ -6,8 +6,8 @@ const BannerCommon = ({ image, title, className = '' }) => {
             style={{ backgroundImage: `url(${image})` }}>
             <div className='absolute inset-0 bg-black bg-opacity-70 flex z-0 justify-center items-center'></div>
             <div className='flex flex-col items-center text-white py-20 md:py-32     lg:py-40 '>
-                <h1 className='text-2xl md:text-7xl lg:text-8xl font-bold font-logo z-[1]'
-                    style={{ fontFamily: `Dancing Script, cursive` }}>{title}</h1>
+                <h1 className='text-2xl md:text-7xl  font-bold  z-[1]'
+                >{title}</h1>
             </div>
         </div>
     );
