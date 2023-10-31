ddf32d8d9a7ae4567ad40e5f74f56aa3c230eff8
Nguyen Truong Son <ntstruongson2k3@gmail.com>
2023-10-31 09:39:27 +0700

    sua connect serrver

--- server/src/server.js
+++ server/src/server.js
@@ -2,7 +2,7 @@ import express from 'express'
 import bodyParser from "body-parser";
 import cors from "cors";
 import { router } from "./routes";
-import connectDB from "./config/connectDB"
+import connectDB from "./connect/connectDB"
 const cookieParser = require('cookie-parser');
 
 // Somewhere in your server.js or main application file
