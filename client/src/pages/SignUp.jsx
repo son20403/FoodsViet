7cf2f1f938f41dd3a60b26a9dbc2e1f35482ad22
hoai <hoai3k4s@gmail.com>
2023-12-20 17:10:32 +0700

    cập nhập 20/12/2023

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,62 +1,115 @@
-import React, { useEffect, useState } from 'react';
-import { Input, InputPassword } from '../components/input';
-import { AtIcon, EmailIcon, UserIcon } from '../components/Icon';
-import { Button } from '../components/button';
-import { yupResolver } from '@hookform/resolvers/yup';
+import React, { useEffect, useState } from "react";
+import { Input, InputPassword } from "../components/input";
+import { AtIcon, EmailIcon, UserIcon } from "../components/Icon";
+import { Button } from "../components/button";
+import { yupResolver } from "@hookform/resolvers/yup";
 import * as Yup from "yup";
-import { useForm } from 'react-hook-form';
-import { useDispatch, useSelector } from 'react-redux';
-import { registerRequest } from '../sagas/auth/authSlice';
-import { getDate } from '../hooks/useGetTime';
+import { useForm } from "react-hook-form";
+import { useDispatch, useSelector } from "react-redux";
+import { registerRequest } from "../sagas/auth/authSlice";
+import { getDate } from "../hooks/useGetTime";
 const schemaValidate = Yup.object({
-    user_name: Yup.string().required("Vui lòng nhập tên đăng nhập!")
-        .max(20, "Tên tài khoản không được dài quá 20 ký tự")
-        .min(6, 'Tên đăng nhập phải lớn hơn 6 kí tự'),
-    full_name: Yup.string().required("Vui lòng nhập họ và tên!")
-        .max(22, "Tên không dài quá 23 ký tự")
-        .min(6, 'Tên đăng nhập phải lớn hơn 6 kí tự'),
-    password: Yup.string()
-        .required("Vui lòng nhập mật khẩu!")
-        .min(6, 'Mật khẩu có ít nhất 8 ký tự!')
-        .max(20, "Mật khẩu không được dài quá 20 ký tự")
-        .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
-            'Mật khẩu cần có ít nhất 1 ký tự in hoa, 1 ký tự thường, 1 số và 1 ký tự đặt biệt!'),
-    re_password: Yup.string()
-        .oneOf([Yup.ref('password'), null], 'Mật khẩu không khớp vui lòng nhập lại!'),
-    email: Yup.string().required("Vui lòng nhập email!").email("Vui lòng nhập đúng định dạng email!"),
-})
+  user_name: Yup.string()
+    .required("Vui lòng nhập tên đăng nhập!")
+    .matches(/^\S*$/, "Không được chứa khoảng trắng")
+    .max(20, "Tên tài khoản không được dài quá 20 ký tự")
+    .min(6, "Tên đăng nhập phải lớn hơn 6 kí tự"),
+  full_name: Yup.string()
+    .required("Vui lòng nhập họ và tên!")
+    .max(22, "Tên không dài quá 23 ký tự")
+    .min(6, "Tên đăng nhập phải lớn hơn 6 kí tự"),
+  password: Yup.string()
+    .required("Vui lòng nhập mật khẩu!")
+    .matches(/^\S*$/, "Không được chứa khoảng trắng")
+    .min(6, "Mật khẩu có ít nhất 8 ký tự!")
+    .max(20, "Mật khẩu không được dài quá 20 ký tự")
+    .matches(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+      "Mật khẩu cần có ít nhất 1 ký tự in hoa, 1 ký tự thường, 1 số và 1 ký tự đặt biệt!"
+    ),
+  re_password: Yup.string().oneOf(
+    [Yup.ref("password"), null],
+    "Mật khẩu không khớp vui lòng nhập lại!"
+  ),
+  email: Yup.string()
+    .required("Vui lòng nhập email!")
+    .email("Vui lòng nhập đúng định dạng email!"),
+});
 const SignUp = () => {
-    const { handleSubmit, formState: { errors, isSubmitting, isValid }, control, reset } =
-        useForm({ resolver: yupResolver(schemaValidate), mode: 'onChange', })
-    const dispatch = useDispatch()
-    const { errorGlobal } = useSelector((state) => state.global)
-    const [error, setError] = useState('');
-    const handleSignUp = (value) => {
-        const date = getDate()
-        if (isValid) {
-            dispatch(registerRequest({ ...value, date, reset }))
-        }
+  const {
+    handleSubmit,
+    formState: { errors, isSubmitting, isValid },
+    control,
+    reset,
+  } = useForm({ resolver: yupResolver(schemaValidate), mode: "onChange" });
+  const dispatch = useDispatch();
+  const { errorGlobal } = useSelector((state) => state.global);
+  const [error, setError] = useState("");
+  const handleSignUp = (value) => {
+    const date = getDate();
+    if (isValid) {
+      dispatch(registerRequest({ ...value, date, reset }));
     }
-    useEffect(() => {
-        setError(errorGlobal)
-    }, [errorGlobal]);
-    return (
-        <form onSubmit={handleSubmit(handleSignUp)} className="sign-up-form form_signin-signup ">
-            <h2 className="title">Đăng Ký</h2>
-            <div className='flex flex-col gap-4 w-full bg-white  p-5 rounded-lg' >
-                <Input control={control} type='text' name={'user_name'} errors={errors} placeholder='Tài khoản'
-                    value='' ><AtIcon /></Input>
-                <Input control={control} type='text' name={'full_name'} errors={errors} placeholder='Họ và tên'
-                    value='' ><UserIcon /></Input>
-                <Input control={control} type='text' name={'email'} errors={errors} placeholder='Email'
-                    value='' ><EmailIcon /></Input>
-                <InputPassword control={control} name={'password'} errors={errors} placeholder='Mật khẩu'
-                    value='' ></InputPassword>
-                <InputPassword control={control} name={'re_password'} errors={errors} placeholder='Nhập lại mật khẩu'
-                    value='' ></InputPassword>
-                <Button isLoading={isSubmitting} type='submit' >Đăng ký</Button>
-            </div>
-        </form>
-    );
+  };
+  useEffect(() => {
+    setError(errorGlobal);
+  }, [errorGlobal]);
+  return (
+    <form
+      onSubmit={handleSubmit(handleSignUp)}
+      className="sign-up-form form_signin-signup "
+    >
+      <h2 className="title">Đăng Ký</h2>
+      <div className="flex flex-col w-full gap-4 p-5 bg-white rounded-lg">
+        <Input
+          control={control}
+          type="text"
+          name={"user_name"}
+          errors={errors}
+          placeholder="Tài khoản"
+          value=""
+        >
+          <AtIcon />
+        </Input>
+        <Input
+          control={control}
+          type="text"
+          name={"full_name"}
+          errors={errors}
+          placeholder="Họ và tên"
+          value=""
+        >
+          <UserIcon />
+        </Input>
+        <Input
+          control={control}
+          type="text"
+          name={"email"}
+          errors={errors}
+          placeholder="Email"
+          value=""
+        >
+          <EmailIcon />
+        </Input>
+        <InputPassword
+          control={control}
+          name={"password"}
+          errors={errors}
+          placeholder="Mật khẩu"
+          value=""
+        ></InputPassword>
+        <InputPassword
+          control={control}
+          name={"re_password"}
+          errors={errors}
+          placeholder="Nhập lại mật khẩu"
+          value=""
+        ></InputPassword>
+        <Button isLoading={isSubmitting} type="submit">
+          Đăng ký
+        </Button>
+      </div>
+    </form>
+  );
 };
 export default SignUp;
--- client/src/pages/adminPage/HomeAdmin.jsx
+++ client/src/pages/adminPage/HomeAdmin.jsx
@@ -1,290 +1,308 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
+import { Typography } from "@material-tailwind/react";
 import {
-    Typography,
-} from "@material-tailwind/react";
-import {
-    NewspaperIcon,
-    Square3Stack3DIcon,
-    UserIcon,
-    ChatBubbleLeftRightIcon,
+  NewspaperIcon,
+  Square3Stack3DIcon,
+  UserIcon,
+  ChatBubbleLeftRightIcon,
 } from "@heroicons/react/24/outline";
-import StatisticsCard from '../../layout/adminLayout/chart/statistics-card';
-import StatisticsChart from '../../layout/adminLayout/chart/statistics-chart';
-import TopCustomerInteract from '../../layout/adminLayout/chart/TopCustomerInteract';
-import { statisticalCategoriesRequest, statisticalCustomersRequest, statisticalFeedbacksRequest, statisticalPostsRequest } from '../../sagas/admin/adminSlice';
+import StatisticsCard from "../../layout/adminLayout/chart/statistics-card";
+import StatisticsChart from "../../layout/adminLayout/chart/statistics-chart";
+import TopCustomerInteract from "../../layout/adminLayout/chart/TopCustomerInteract";
+import {
+  statisticalCategoriesRequest,
+  statisticalCustomersRequest,
+  statisticalFeedbacksRequest,
+  statisticalPostsRequest,
+} from "../../sagas/admin/adminSlice";
 const HomeAdmin = () => {
-    const { statistical_customers, statistical_feedbacks, statistical_posts,
-        statistical_categories, posts, categories, customers } =
-        useSelector((state) => state.admin);
-    const { feedback } = useSelector((state) => state.feedback)
-    const statisticalByDayCustomer = statistical_customers?.statisticalByMonth
-    const statisticalByDayFeedback = statistical_feedbacks?.statisticalByMonth
-    const statisticalByDayPost = statistical_posts?.statisticalByMonth
-    const statisticalByDayCategory = statistical_categories?.statisticalByMonth
-    const totalPosts = posts?.length || 0
-    const totalCategories = categories?.length || 0
-    const totalFeedbacks = feedback?.length || 0
-    const totalCustomers = customers?.length || 0
-    let dayByCustomer = []
-    let dataByCustomer = []
-    let dayByFeedback = []
-    let dataByFeedback = []
-    let dayByPost = []
-    let dataByPost = []
-    let dayByCategory = []
-    let dataByCategory = []
-    const months = ['T1', 'T2', 'T3', 'T4', 'T5', 'T6', 'T7', 'T8', 'T9', 'T10', 'T11', 'T12']
-    pushData(statisticalByDayCustomer, dayByCustomer, dataByCustomer)
-    pushData(statisticalByDayFeedback, dayByFeedback, dataByFeedback)
-    pushData(statisticalByDayPost, dayByPost, dataByPost)
-    pushData(statisticalByDayCategory, dayByCategory, dataByCategory)
-    function pushData(statiscal, day, data) {
-        statiscal?.map((item) => {
-            day.push(item?._id.slice(5))
-        })
-        statiscal?.map((item) => {
-            data.push(item?.count)
-        })
-    }
-    const dispatch = useDispatch()
-    useEffect(() => {
-        dispatch(statisticalCustomersRequest({ model: 'customer' }))
-        dispatch(statisticalFeedbacksRequest({ model: 'feedback' }))
-        dispatch(statisticalPostsRequest({ model: 'post' }))
-        dispatch(statisticalCategoriesRequest({ model: 'category' }))
-    }, []);
-    const feedbackChart = {
-        type: "line",
-        height: 220,
-        series: [
-            {
-                name: "Phản hồi",
-                data: dataByFeedback,
-            },
-        ],
-        options: {
-            ...chartsConfig,
-            colors: ["#fff"],
-            stroke: {
-                lineCap: "round",
-            },
-            markers: {
-                size: 5,
-            },
-            xaxis: {
-                ...chartsConfig.xaxis,
-                categories: months,
-            },
-        },
-    };
-    const customerChart = {
-        type: "line",
-        height: 220,
-        series: [
-            {
-                name: "Người dùng",
-                data: dataByCustomer,
-            },
-        ],
-        options: {
-            ...chartsConfig,
-            colors: ["#fff"],
-            stroke: {
-                lineCap: "round",
-            },
-            markers: {
-                size: 5,
-            },
-            xaxis: {
-                ...chartsConfig.xaxis,
-                categories: months,
-            },
-        },
-    };
-    const postChart = {
-        type: "line",
-        height: 220,
-        series: [
-            {
-                name: "Bài viết",
-                data: dataByPost,
-            },
-        ],
-        options: {
-            ...chartsConfig,
-            colors: ["#fff"],
-            stroke: {
-                lineCap: "round",
-            },
-            markers: {
-                size: 5,
-            },
-            xaxis: {
-                ...chartsConfig.xaxis,
-                categories: months,
-            },
-        },
-    };
-    const categoryChart = {
-        type: "line",
-        height: 220,
-        series: [
-            {
-                name: "Danh mục",
-                data: dataByCategory,
-            },
-        ],
-        options: {
-            ...chartsConfig,
-            colors: ["#fff"],
-            stroke: {
-                lineCap: "round",
-            },
-            markers: {
-                size: 5,
-            },
-            xaxis: {
-                ...chartsConfig.xaxis,
-                categories: months,
-            },
-        },
-    };
-    const statisticsChartsData = [
-        {
-            color: "blue",
-            title: "Thống kê bài viết",
-            chart: postChart,
-        },
-        {
-            color: "pink",
-            title: "Thống kê người dùng",
-            chart: customerChart,
-        },
-        {
-            color: "green",
-            title: "Thống kê danh mục",
-            chart: categoryChart,
-        },
-        {
-            color: "orange",
-            title: "Thống kê phản hồi",
-            chart: feedbackChart,
-        },
-    ];
-    const statisticsCardsData = [
-        {
-            color: "blue",
-            icon: NewspaperIcon,
-            title: "Bài viết",
-            value: totalPosts,
-        },
-        {
-            color: "pink",
-            icon: UserIcon,
-            title: "Người dùng",
-            value: totalCustomers,
-        },
-        {
-            color: "green",
-            icon: Square3Stack3DIcon,
-            title: "Danh mục",
-            value: totalCategories,
-        },
-        {
-            color: "orange",
-            icon: ChatBubbleLeftRightIcon,
-            title: "Phản hồi",
-            value: totalFeedbacks,
-        },
-    ];
-    return (
-        <div className="mt-12">
-            <div className="mb-12 grid gap-y-10 gap-x-6 md:grid-cols-2 xl:grid-cols-4">
-                {statisticsCardsData.map(({ icon, title, ...rest }) => (
-                    <StatisticsCard
-                        key={title}
-                        {...rest}
-                        title={title}
-                        icon={React.createElement(icon, {
-                            className: "w-6 h-6 text-white",
-                        })}
-                    />
-                ))}
-            </div>
-            <div className="mb-6 grid grid-cols-1 gap-y-12 gap-x-6 md:grid-cols-2 ">
-                {statisticsChartsData.map((props) => (
-                    <StatisticsChart
-                        key={props.title}
-                        {...props}
-                    />
-                ))}
-            </div>
-            <TopCustomerInteract></TopCustomerInteract>
-        </div>
-    );
+  const {
+    statistical_customers,
+    statistical_feedbacks,
+    statistical_posts,
+    statistical_categories,
+    posts,
+    categories,
+    customers,
+  } = useSelector((state) => state.admin);
+  const { feedback } = useSelector((state) => state.feedback);
+  const statisticalByDayCustomer = statistical_customers?.statisticalByMonth;
+  const statisticalByDayFeedback = statistical_feedbacks?.statisticalByMonth;
+  const statisticalByDayPost = statistical_posts?.statisticalByMonth;
+  const statisticalByDayCategory = statistical_categories?.statisticalByMonth;
+  const totalPosts = posts?.length || 0;
+  const totalCategories = categories?.length || 0;
+  const totalFeedbacks = feedback?.length || 0;
+  const totalCustomers = customers?.length || 0;
+  let dayByCustomer = [];
+  let dataByCustomer = [];
+  let dayByFeedback = [];
+  let dataByFeedback = [];
+  let dayByPost = [];
+  let dataByPost = [];
+  let dayByCategory = [];
+  let dataByCategory = [];
+  const months = [
+    "T1",
+    "T2",
+    "T3",
+    "T4",
+    "T5",
+    "T6",
+    "T7",
+    "T8",
+    "T9",
+    "T10",
+    "T11",
+    "T12",
+  ];
+  pushData(statisticalByDayCustomer, dayByCustomer, dataByCustomer);
+  pushData(statisticalByDayFeedback, dayByFeedback, dataByFeedback);
+  pushData(statisticalByDayPost, dayByPost, dataByPost);
+  pushData(statisticalByDayCategory, dayByCategory, dataByCategory);
+  function pushData(statiscal, day, data) {
+    statiscal?.map((item) => {
+      day.push(item?._id?.slice(5));
+    });
+    statiscal?.map((item) => {
+      data.push(item?.count);
+    });
+  }
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(statisticalCustomersRequest({ model: "customer" }));
+    dispatch(statisticalFeedbacksRequest({ model: "feedback" }));
+    dispatch(statisticalPostsRequest({ model: "post" }));
+    dispatch(statisticalCategoriesRequest({ model: "category" }));
+  }, []);
+  const feedbackChart = {
+    type: "line",
+    height: 220,
+    series: [
+      {
+        name: "Phản hồi",
+        data: dataByFeedback,
+      },
+    ],
+    options: {
+      ...chartsConfig,
+      colors: ["#fff"],
+      stroke: {
+        lineCap: "round",
+      },
+      markers: {
+        size: 5,
+      },
+      xaxis: {
+        ...chartsConfig.xaxis,
+        categories: months,
+      },
+    },
+  };
+  const customerChart = {
+    type: "line",
+    height: 220,
+    series: [
+      {
+        name: "Người dùng",
+        data: dataByCustomer,
+      },
+    ],
+    options: {
+      ...chartsConfig,
+      colors: ["#fff"],
+      stroke: {
+        lineCap: "round",
+      },
+      markers: {
+        size: 5,
+      },
+      xaxis: {
+        ...chartsConfig.xaxis,
+        categories: months,
+      },
+    },
+  };
+  const postChart = {
+    type: "line",
+    height: 220,
+    series: [
+      {
+        name: "Bài viết",
+        data: dataByPost,
+      },
+    ],
+    options: {
+      ...chartsConfig,
+      colors: ["#fff"],
+      stroke: {
+        lineCap: "round",
+      },
+      markers: {
+        size: 5,
+      },
+      xaxis: {
+        ...chartsConfig.xaxis,
+        categories: months,
+      },
+    },
+  };
+  const categoryChart = {
+    type: "line",
+    height: 220,
+    series: [
+      {
+        name: "Danh mục",
+        data: dataByCategory,
+      },
+    ],
+    options: {
+      ...chartsConfig,
+      colors: ["#fff"],
+      stroke: {
+        lineCap: "round",
+      },
+      markers: {
+        size: 5,
+      },
+      xaxis: {
+        ...chartsConfig.xaxis,
+        categories: months,
+      },
+    },
+  };
+  const statisticsChartsData = [
+    {
+      color: "blue",
+      title: "Thống kê bài viết",
+      chart: postChart,
+    },
+    {
+      color: "pink",
+      title: "Thống kê người dùng",
+      chart: customerChart,
+    },
+    {
+      color: "green",
+      title: "Thống kê danh mục",
+      chart: categoryChart,
+    },
+    {
+      color: "orange",
+      title: "Thống kê phản hồi",
+      chart: feedbackChart,
+    },
+  ];
+  const statisticsCardsData = [
+    {
+      color: "blue",
+      icon: NewspaperIcon,
+      title: "Bài viết",
+      value: totalPosts,
+    },
+    {
+      color: "pink",
+      icon: UserIcon,
+      title: "Người dùng",
+      value: totalCustomers,
+    },
+    {
+      color: "green",
+      icon: Square3Stack3DIcon,
+      title: "Danh mục",
+      value: totalCategories,
+    },
+    {
+      color: "orange",
+      icon: ChatBubbleLeftRightIcon,
+      title: "Phản hồi",
+      value: totalFeedbacks,
+    },
+  ];
+  return (
+    <div className="mt-12">
+      <div className="grid mb-12 gap-y-10 gap-x-6 md:grid-cols-2 xl:grid-cols-4">
+        {statisticsCardsData.map(({ icon, title, ...rest }) => (
+          <StatisticsCard
+            key={title}
+            {...rest}
+            title={title}
+            icon={React.createElement(icon, {
+              className: "w-6 h-6 text-white",
+            })}
+          />
+        ))}
+      </div>
+      <div className="grid grid-cols-1 mb-6 gap-y-12 gap-x-6 md:grid-cols-2 ">
+        {statisticsChartsData.map((props) => (
+          <StatisticsChart key={props.title} {...props} />
+        ))}
+      </div>
+      <TopCustomerInteract></TopCustomerInteract>
+    </div>
+  );
 };
 
 export default HomeAdmin;
 
 const chartsConfig = {
-    chart: {
-        toolbar: {
-            show: false,
-        },
+  chart: {
+    toolbar: {
+      show: false,
     },
-    title: {
-        show: "",
+  },
+  title: {
+    show: "",
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  xaxis: {
+    axisTicks: {
+      show: true,
     },
-    dataLabels: {
-        enabled: false,
+    axisBorder: {
+      show: true,
     },
+    labels: {
+      style: {
+        colors: "#fff",
+        fontSize: "12px",
+        fontFamily: "inherit",
+        fontWeight: 300,
+      },
+    },
+  },
+  yaxis: {
+    labels: {
+      style: {
+        colors: "#fff",
+        fontSize: "13px",
+        fontFamily: "inherit",
+        fontWeight: 300,
+      },
+    },
+  },
+  grid: {
+    show: true,
+    borderColor: "#ffffff40",
+    strokeDashArray: 5,
     xaxis: {
-        axisTicks: {
-            show: true,
-        },
-        axisBorder: {
-            show: true,
-        },
-        labels: {
-            style: {
-                colors: "#fff",
-                fontSize: "12px",
-                fontFamily: "inherit",
-                fontWeight: 300,
-            },
-        },
-    },
-    yaxis: {
-        labels: {
-            style: {
-                colors: "#fff",
-                fontSize: "13px",
-                fontFamily: "inherit",
-                fontWeight: 300,
-            },
-        },
-    },
-    grid: {
+      lines: {
         show: true,
-        borderColor: "#ffffff40",
-        strokeDashArray: 5,
-        xaxis: {
-            lines: {
-                show: true,
-            },
-        },
-        padding: {
-            top: 5,
-            right: 20,
-        },
+      },
     },
-    fill: {
-        opacity: 0.8,
+    padding: {
+      top: 5,
+      right: 20,
     },
-    tooltip: {
-        theme: "dark",
-    },
+  },
+  fill: {
+    opacity: 0.8,
+  },
+  tooltip: {
+    theme: "dark",
+  },
 };
-
